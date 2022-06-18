316d333ca21f5a09036b6c199062ba3c6fe31ba8
diana-dzindule <diana.dzindule@gmail.com>
2022-06-18 14:18:48 +0300

    Update FirstClass.java

--- src/main/java/Homework/FirstClass.java
+++ src/main/java/Homework/FirstClass.java
@@ -4,7 +4,9 @@ public class FirstClass {
     public static void main(String[] args) {
         String valsts = "Šveice";
         int iedzivotajuskaits = 8570700;
-        int platiba = 41284;
+        double platiba = 41.3;
+        String tukstos = "tūktoš";
+        byte m2 = 2;
         String galvaspilseta = " Berne";
         byte valoduSkaits = 4;
         String oficialasValodas = " vācu, franču, itāļu, retoromāņu";
@@ -13,14 +15,22 @@ public class FirstClass {
         boolean vaiIrESDaibvalsts = false;
         String oficialaValuta = "CHF";
         char currency = '€';
+        char punkts = '.';
+        char atstarpe = ' ';
+        int viens = 1;
 
-        System.out.println("Valsts: " + valsts);
-        System.out.println("Iedzīvotāju skaits: " + iedzivotajuskaits);
-        System.out.println("Plaība: " + platiba);
-        System.out.println("Galvasilsēta: " + valsts + daudzskaitlis + galvaspilseta);
-        System.out.println("Vai Šveice ir Eiropas Savienības dalībvalsts?: " + vaiIrESDaibvalsts);
-        System.out.println("Šveicē ir " + valoduSkaits +  "oficiālās " + vardsValodas + oficialasValodas + vardsValodas);
-        System.out.println("Šveices valūta ir: " + oficialaValuta + ", nevar izmantot " + currency);
+        System.out.println("Valsts: " + valsts + punkts);
+        System.out.println("Iedzīvotāju skaits: " + iedzivotajuskaits + punkts);
+        System.out.println("Platība: " + platiba + punkts);
+        System.out.println("Galvaspilsēta: " + valsts + daudzskaitlis + galvaspilseta + punkts);
+        System.out.println("Vai Šveice ir Eiropas Savienības dalībvalsts?: " + vaiIrESDaibvalsts + punkts);
 
+
+
+        System.out.println(valsts + atstarpe + "ir skaista" + punkts);
+        System.out.println(valsts + daudzskaitlis + atstarpe + "platība ir" + atstarpe + platiba + atstarpe + tukstos + "m" + m2 + punkts);
+        System.out.println("Šveicē ir " + valoduSkaits + atstarpe + "oficiālās " + vardsValodas + oficialasValodas + vardsValodas + punkts);
+        System.out.println(valsts + daudzskaitlis + atstarpe + "valūta ir:" + atstarpe + oficialaValuta + ", nevar izmantot " + currency + punkts);
+        System.out.println(valsts + vaiIrESDaibvalsts + punkts);
     }
 }
